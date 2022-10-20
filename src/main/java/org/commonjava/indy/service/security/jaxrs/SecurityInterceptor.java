bff56811ac34c8aafeb0aaf0dcb63331c72506a4
Gang Li <ligangty@users.noreply.github.com>
2022-10-20 11:18:39 +0800

    Fix: add enablement check in SecurityInterceptor

--- src/main/java/org/commonjava/indy/service/security/jaxrs/SecurityInterceptor.java
+++ src/main/java/org/commonjava/indy/service/security/jaxrs/SecurityInterceptor.java
@@ -15,11 +15,11 @@
  */
 package org.commonjava.indy.service.security.jaxrs;
 
+import org.commonjava.indy.service.security.common.SecurityConfiguration;
 import org.commonjava.indy.service.security.common.SecurityManager;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Instance;
 import javax.inject.Inject;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
@@ -27,33 +27,34 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.ext.Provider;
-import java.io.IOException;
 
 @ApplicationScoped
 @Provider
 public class SecurityInterceptor
         implements ContainerRequestFilter
 {
-    private final Logger logger = LoggerFactory.getLogger( this.getClass() );
-
     @Inject
     SecurityManager securityManager;
 
     @Context
     UriInfo info;
 
+    @Inject
+    Instance<SecurityConfiguration> config;
+
     @Override
     public void filter( ContainerRequestContext requestContext )
-            throws IOException
     {
-
-        final String path = info.getPath();
-
-        final String method = requestContext.getMethod();
-
-        if ( !securityManager.authorized( path, method ) )
+        if ( config.get().enabled() )
         {
-            requestContext.abortWith( Response.status( Response.Status.FORBIDDEN ).build() );
+            final String path = info.getPath();
+
+            final String method = requestContext.getMethod();
+
+            if ( !securityManager.authorized( path, method ) )
+            {
+                requestContext.abortWith( Response.status( Response.Status.FORBIDDEN ).build() );
+            }
         }
     }
 }
---
+++ src/test/java/org/commonjava/indy/service/security/jaxrs/SecurityDisabledInResourceTest.java
@@ -0,0 +1,60 @@
+/**
+ * Copyright (C) 2022 Red Hat, Inc. (https://github.com/Commonjava/indy-security)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.commonjava.indy.service.security.jaxrs;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.TestProfile;
+import io.quarkus.test.security.TestSecurity;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static javax.ws.rs.core.Response.Status.CREATED;
+import static javax.ws.rs.core.Response.Status.FORBIDDEN;
+import static javax.ws.rs.core.Response.Status.NO_CONTENT;
+import static javax.ws.rs.core.Response.Status.OK;
+
+@QuarkusTest
+@TestProfile( SecurityDisabledProfile.class )
+public class SecurityDisabledInResourceTest
+{
+    @Test
+    public void testPublic()
+    {
+        given().when().get( "/api/public" ).then().statusCode( OK.getStatusCode() );
+    }
+
+    @Test
+    public void testWriteAdmin()
+    {
+        given().when().put( "/api/admin/resource" ).then().statusCode( CREATED.getStatusCode() );
+        given().when().post( "/api/admin/resource" ).then().statusCode( CREATED.getStatusCode() );
+        given().when().delete( "/api/admin/resource" ).then().statusCode( NO_CONTENT.getStatusCode() );
+    }
+
+    @Test
+    public void testGetStore()
+    {
+        given().when().get( "/api/admin/stores/testtype/test" ).then().statusCode( OK.getStatusCode() );
+    }
+
+    @Test
+    public void testWriteStore()
+    {
+        given().when().put( "/api/admin/stores/testtype/test" ).then().statusCode( CREATED.getStatusCode() );
+        given().when().post( "/api/admin/stores/testtype/test" ).then().statusCode( CREATED.getStatusCode() );
+        given().when().delete( "/api/admin/stores/testtype/test" ).then().statusCode( NO_CONTENT.getStatusCode() );
+    }
+}
